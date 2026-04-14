ad428586408d2ce6aba46f5334945570198545d2
estefanitusalazar2 <estefania.iturrieta02@inacapmail.cl>
2026-04-14 14:18:31 -0400

    random

---
+++ Clase 14 de abril.py
@@ -0,0 +1,12 @@
+Import random
+
+Print ("CALCULADORA")
+Print("1] suma")
+Print("2] resta")
+Print("3] multiplicacion")
+Print("4] division")
+opc= int(input("inrgese una opcion:"))
+
+num1= random.randint(100, 200)
+num2=random. randint(100, 200)
+
