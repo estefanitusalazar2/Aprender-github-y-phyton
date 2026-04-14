e2c4795fadb2562bb15f013b391a11bd6bd5e533
estefanitusalazar2 <estefania.iturrieta02@inacapmail.cl>
2026-04-14 13:51:15 -0400

    Update Aprender phyton.py

--- Aprender phyton.py
+++ Aprender phyton.py
@@ -45,3 +45,56 @@ print(nombre[0:2] + nombre[-1].lower() + "." + apellido[0].lower() + str(anio_na
 #mas la primera letra del apellido
 #mas el año de nacimiento
 #@inacapmail.cl
+
+
+
+
+
+#ingresando el lower
+
+nombre = "Estefania"
+
+# PROPIEDADES DEL str
+print(nombre)
+
+# Mayúscula
+print(nombre.upper())
+
+# Minúscula 
+print(nombre.lower())
+
+# El largo
+print(len(nombre))
+
+# La primera letra
+print(nombre[0])
+
+# La última letra
+print(nombre[-1])
+
+# Las primeras 3 letras
+print(nombre[0] + nombre[1] + nombre[2])
+print(nombre[0:3])
+
+
+
+#print (nombre [1:3])
+print(nombre[1:3])
+# #print(nombre[3:3])
+print(nombre[3:3])
+
+#Solicitar el nombre, apellido, edad
+nombre = input("Ingrese su nombre: ").lower()
+apellido = input("Ingrese su apellido: ").lower()
+edad = input("Ingrese su edad: ")
+anio_nacimiento = 2026 - int(edad)
+
+#crea el correo institucional. Todo en minuscula
+print(nombre[0:2] + nombre[-1]+ "." + apellido[0] + str(anio_nacimiento) + "@inacapmail.cl")
+
+#Las primeras 2 letras del nombre
+#la ultima letra del nombre
+#mas un "."
+#mas la primera letra del apellido
+#mas el año de nacimiento
+#@inacapmail.cl
