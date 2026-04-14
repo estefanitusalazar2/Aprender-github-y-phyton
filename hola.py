716a2ddda848ad8edaa693861cf3a1f0bceb6676
estefanitusalazar2 <estefania.iturrieta02@inacapmail.cl>
2026-04-14 13:45:41 -0400

    print redondeos en phyon

---
+++ hola.py
@@ -0,0 +1,11 @@
+print (str (10/3))
+#Phyton suele poner todos los decimales, hay que redondear por lo tanto...
+
+resultado= 10/3
+print(f"{resultado: .2f}")
+print (f"{resultado: .0%}")
+
+
+#2f sirve para hacer un redondeo si es sobre 6, si es 5 y segundo decimal no redondea el 5
+
+
